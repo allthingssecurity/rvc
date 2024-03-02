06eaa12427256206f110ddc0ddd486d400133b8f
Jain <shashank.jain01@sap.com>
2024-03-02 15:35:11 +0530

    handling exception

--- train_nsf_sim_cache_sid_load_pretrain.py
+++ train_nsf_sim_cache_sid_load_pretrain.py
@@ -526,8 +526,8 @@ def train_and_evaluate(
             "saving final ckpt:%s"
             % (savee(ckpt, hps.sample_rate, hps.if_f0, hps.name, epoch))
         )
-        sleep(1)
-        os._exit(2333333)
+        #sleep(1)
+        #os._exit(2333333)
 
 
 if __name__ == "__main__":

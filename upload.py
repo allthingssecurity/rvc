ff457db74216e9b131a268608d0b63f7eb1d16ad
Jain <shashank.jain01@sap.com>
2024-03-05 13:31:17 +0530

    changes to upload and train file

--- upload.py
+++ upload.py
@@ -6,7 +6,7 @@ ACCESS_ID = os.getenv('ACCESS_ID', 'default_value')
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_value')
 
 
-def upload_to_do(file_path):
+def upload_to_do(file_path,spk_id):
     # Configure Cloudinary with your credentials
     session = session.Session()
     client = session.client('s3',
@@ -16,7 +16,7 @@ def upload_to_do(file_path):
                             aws_secret_access_key=SECRET_KEY)
 
     # Upload a file to your Space
-    response=client.upload_file('c:/shashank/trips/test.mp3', 'sing', 'sing/test.mp3')
+    response=client.upload_file(file_path, 'sing', file_path)
 
     
     return response

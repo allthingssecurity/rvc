2818195f32cc6ab8179f0667c8e48fb58e145926
Jain <shashank.jain01@sap.com>
2024-03-05 13:34:56 +0530

    changes to upload and train file

--- upload.py
+++ upload.py
@@ -8,8 +8,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_value')
 
 def upload_to_do(file_path,spk_id):
     # Configure Cloudinary with your credentials
-    session = session.Session()
-    client = session.client('s3',
+    boto_session=session = session.Session()
+    client = boto_session.client('s3',
                             region_name='nyc3',
                             endpoint_url='https://nyc3.digitaloceanspaces.com',
                             aws_access_key_id=ACCESS_ID,

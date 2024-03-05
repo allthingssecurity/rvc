1a38eeafca820faf55a69b5986263e739cec1cea
Jain <shashank.jain01@sap.com>
2024-03-05 13:19:42 +0530

    changes to upload and train file

--- trainendpoint.py
+++ trainendpoint.py
@@ -51,7 +51,7 @@ def process_audio():
         subprocess.run(['python', 'extract_f0_print.py', log_dir, '12', 'harvest'], check=True)
         subprocess.run(['python', 'extract_feature_print.py', 'cuda:0', '1', '0', '0', log_dir], check=True)
         subprocess.run(['python', 'pretrain.py', '--exp_dir1', model_name, '--sr2', '40k', '--if_f0_3', '1', '--spk_id5', '0'], check=True)
-        subprocess.run(['python3', 'train_nsf_sim_cache_sid_load_pretrain.py', '-e', model_n*ame, '-sr', '40k', '-f0', '1', '-bs', '19', '-g', '0', '-te', '20', '-se', '20', '-pg', 'pretrained/f0G40k.pth', '-pd', 'pretrained/f0D40k.pth', '-l', '0', '-c', '0'], check=True)
+        subprocess.run(['python3', 'train_nsf_sim_cache_sid_load_pretrain.py', '-e', model_name, '-sr', '40k', '-f0', '1', '-bs', '19', '-g', '0', '-te', '20', '-se', '20', '-pg', 'pretrained/f0G40k.pth', '-pd', 'pretrained/f0D40k.pth', '-l', '0', '-c', '0'], check=True)
 
         return jsonify({'success': True, 'message': 'Files processed successfully.'})
     except Exception as e:

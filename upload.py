3d8d83c5d5797a545d98db00757b987ea49993ba
Jain <shashank.jain01@sap.com>
2024-03-05 13:15:22 +0530

    changes to upload and train file

--- upload.py
+++ upload.py
@@ -1,16 +1,12 @@
 # cloudinary_upload.py
 from boto3 import session
 from botocore.client import Config
-
 import os
-from boto3 import session
-from botocore.client import Config
-
 ACCESS_ID = os.getenv('ACCESS_ID', 'default_value')
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_value')
 
 
-def upload_to_do(file_path,spk_id):
+def upload_to_do(file_path):
     # Configure Cloudinary with your credentials
     session = session.Session()
     client = session.client('s3',
@@ -20,7 +16,7 @@ def upload_to_do(file_path,spk_id):
                             aws_secret_access_key=SECRET_KEY)
 
     # Upload a file to your Space
-    response=client.upload_file(file_path, spk_id, f'sing/{spk_id}')
+    response=client.upload_file('c:/shashank/trips/test.mp3', 'sing', 'sing/test.mp3')
 
     
     return response
